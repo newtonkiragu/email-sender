a4c28aba5027e0f13a65541ba17a02710f9e7297
anampiu <anampiuandrew4@gmial.com>
2019-01-10 17:17:00 +0300

    classroom credentials

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-
 const csv= require("csvtojson")
 const nodemailer = require('nodemailer');
 var smtpTransport = require('nodemailer-smtp-transport');
@@ -21,8 +20,32 @@ var sendMail= function(d){
   var mailOptions = {
     from: authEmail.user,
     to: d["Email"],
-    subject:  `Scores`,
-    text: `Hi ${d["Name"]}, your score is ${d["Score"]}`
+    subject:  `A Quick Status Update`,
+    text: `
+Hello ${d["Name"]},
+
+To give you an update on how you are doing in class, you are currently on track to move forward to the next module.
+
+As you know, Moringa School looks at 3 major aspects of your learning in considering whether you proceed to the next module:
+Attendance and punctuality.
+Completion and quality of your IPs.
+Positive contributions to the classroom environment; are you working well with others, and interpersonal skills.
+
+As it stands:
+Your attendance out of 100 is ${d["Attendance"]}.
+
+As for your IPs:
+IP1 out of 22 you have ${d["IP1 /31"]}.
+IP2 out of 31 you have ${d["IP2 /28"]}.
+IP3 out of 100% you have ${d["IP3 /100%"]}.
+
+Based on the above scores we recommend that you spend the rest of the module working on keeping up with your attendance, IP submissions and quality, and your interpersonal dynamics. Your work has been good so far, and we want to encourage you to keep the momentum going.
+
+This is an automatic email. Please do not reply to it. Follow up with your TM if you have any questions and fill out this form.
+    
+Best,
+-Classroom Team
+    `
   };
   transporter.sendMail(mailOptions, function(error, info){
     if (error) {
@@ -36,7 +59,7 @@ var sendMail= function(d){
 
 
 
-csv().fromFile('sample.csv').then((j)=>{
+csv().fromFile('data.csv').then((j)=>{
     const data=j
     for (i=0; i<data.length;i++){
       sendMail(data[i])
