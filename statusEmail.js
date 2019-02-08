5d6afdcbb3a1c845f4fd11f63d12101d186fb99d
anampiu <anampiuandrew4@gmail.com>
2019-02-08 11:50:40 +0300

    combining both files

--- statusEmail.js
+++ statusEmail.js
@@ -16,13 +16,47 @@ var transporter = nodemailer.createTransport(smtpTransport({
   auth: authEmail
 }))
 
+//Different emails sent for different criteria
+
+var notOnTrack = `
+
+`
+
+
 var sendMail= function(d){
-  var mailOptions = {
-    from: authEmail.user,
-    to: d["Email"],
-    cc: ['andrew.anampiu@moringaschool.com','boyd.ndonga@moringaschool.com','ashley.zawadi@moringaschool.com','samora.yommie@moringaschool.com'],
-    subject:  `A Quick Status Update`,
-    text: `
+    //Check for yes or no on recommendation
+  if (d['First Recommendation']==='Yes'){
+      text=`
+Hello ${d["Student"]}
+
+To give you an update on how you are doing in class, you are currently on track to move forward to the next module.
+    
+As you know, Moringa School looks at 3 major aspects of your learning in considering whether you proceed to the next module:
+  Attendance and punctuality.
+  Completion and quality of your IPs.
+  Positive contributions to the classroom environment; are you working well with others, and interpersonal skills.
+    
+As it stands:
+  Your attendance out of 100 is ${d["Attendance /100"]}.
+    
+As for your IPs:
+Angular
+  IP1 out of 31 you have ${d["IP1 /31"]}.
+  IP2 out of 28 you have ${d["IP2 /28"]}.
+Python
+  IP1 out of 21 you have ${d["IP3 /21"]}.
+  IP2 out of 22 you have ${d["IP4 /22"]}.
+    
+Based on the above scores we recommend that you spend the rest of the module working on keeping up with your attendance, IP submissions and quality, and your interpersonal dynamics. Your work has been good so far, and we want to encourage you to keep the momentum going.
+    
+This is an automatic email. Please do not reply to it. Follow up with your TM if you have any questions and fill out this form.
+https://goo.gl/forms/0IirlKjvqZ7Qwyt72
+
+Best,
+-Classroom Team.
+      `;
+  }else{
+      text=`
 Hello ${d["Student"]}
 
 To give you an update on how you are doing in class, you are currently not on track to move forward to the next module but have an opportunity to improve before final decisions are made. Please continue reading to understand why this is, and how you can improve.
@@ -49,8 +83,17 @@ https://goo.gl/forms/0IirlKjvqZ7Qwyt72
 
 Best,
 -Classroom Team.
-    `
+      `;
+  }
+  //email options
+  var mailOptions = {
+    from: authEmail.user,
+    to: d["Email"],
+    cc: ['andrew.anampiu@moringaschool.com','boyd.ndonga@moringaschool.com','ashley.zawadi@moringaschool.com','samora.yommie@moringaschool.com'],
+    subject:  `A Quick Status Update`,
+    text: text
   };
+  //Error logging
   transporter.sendMail(mailOptions, function(error, info){
     if (error) {
       console.log(error);
@@ -61,8 +104,7 @@ Best,
   
 }
 
-
-
+//Read from CSV and send emails
 csv().fromFile('data.csv').then((j)=>{
     const data=j
     for (i=0; i<data.length;i++){
