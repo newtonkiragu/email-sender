3c04e3c09903616ca444f22cf34c4d425dbc6da3
jakhax <jackogina60@gmail.com>
2019-02-28 17:22:49 +0300

    simple student parser

--- mailer_python/parsers/__init__.py
+++ mailer_python/parsers/__init__.py
@@ -1,5 +1,7 @@
 from .simple_parser import SimpleCsvEmailParser
+from .students_review_parser import StudentReviewParser
 
 csv_parsers={
     "SimpleParser":SimpleCsvEmailParser,
+    "StudentReview":StudentReviewParser,
 }
